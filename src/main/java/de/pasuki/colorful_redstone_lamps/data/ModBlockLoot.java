71dc8da324f9521b37ba9c4fe0ccd7b93eb4272e
PasukiTV <Pascal-Smigielski@outlook.de>
2026-02-14 02:23:47 +0100

    Generate self-drop loot tables for all lamp blocks

--- src/main/java/de/pasuki/colorful_redstone_lamps/data/ModBlockLoot.java
+++ src/main/java/de/pasuki/colorful_redstone_lamps/data/ModBlockLoot.java
@@ -2,29 +2,24 @@ package de.pasuki.colorful_redstone_lamps.data;
 
 import de.pasuki.colorful_redstone_lamps.block.ModBlocks;
 import java.util.Set;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.registries.DeferredBlock;
 
 public class ModBlockLoot extends BlockLootSubProvider {
     public ModBlockLoot(HolderLookup.Provider lookup) {
         super(Set.of(), FeatureFlags.REGISTRY.allFlags(), lookup);
     }
 
+    @Override
     protected void generate() {
-        this.streamAllBlocks().forEach((b) -> this.dropSelf(b));
+        getKnownBlocks().forEach(this::dropSelf);
     }
 
+    @Override
     protected Iterable<Block> getKnownBlocks() {
-        return this.streamAllBlocks().toList();
-    }
-
-    private Stream<Block> streamAllBlocks() {
-        Stream<Block> normal = ModBlocks.LAMPS.values().stream().map(Supplier::get);
-        Stream<Block> inverted = ModBlocks.INVERTED_LAMPS.values().stream().map(Supplier::get);
-        return Stream.concat(normal, inverted);
+        return ModBlocks.BLOCKS.getEntries().stream().map(DeferredBlock::get).toList();
     }
 }
