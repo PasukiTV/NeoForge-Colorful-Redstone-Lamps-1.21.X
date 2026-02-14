9746c1ea12602af2cb48de7b20f00c1fb737dd0e
PasukiTV <Pascal-Smigielski@outlook.de>
2026-02-14 02:15:45 +0100

    Clean up client initializer and remove unused event subscriber code

--- src/main/java/de/pasuki/colorful_redstone_lamps/ColorfulRedstoneLampsClient.java
+++ src/main/java/de/pasuki/colorful_redstone_lamps/ColorfulRedstoneLampsClient.java
@@ -1,30 +1,17 @@
 package de.pasuki.colorful_redstone_lamps;
 
-import net.neoforged.api.distmarker.Dist;
 import de.pasuki.colorful_redstone_lamps.client.TooltipHandler;
-import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 
-@Mod(
-        value = "colorful_redstone_lamps",
-        dist = {Dist.CLIENT}
-)
-@EventBusSubscriber(
-        modid = "colorful_redstone_lamps"
-)
+@Mod(value = ColorfulRedstoneLamps.MOD_ID, dist = Dist.CLIENT)
 public class ColorfulRedstoneLampsClient {
     public ColorfulRedstoneLampsClient(ModContainer container) {
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
         NeoForge.EVENT_BUS.addListener(TooltipHandler::onTooltip);
-    }
-
-    @SubscribeEvent
-    static void onClientSetup(FMLClientSetupEvent event) {
     }
 }
