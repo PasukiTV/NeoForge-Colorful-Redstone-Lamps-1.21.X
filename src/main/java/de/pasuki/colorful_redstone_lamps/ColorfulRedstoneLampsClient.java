2ef80fb5f4330ecff3e88b53d11b1c9f63997727
PasukiTV <Pascal-Smigielski@outlook.de>
2026-02-14 02:39:52 +0100

    Fix tooltip listener type inference on event bus

--- src/main/java/de/pasuki/colorful_redstone_lamps/ColorfulRedstoneLampsClient.java
+++ src/main/java/de/pasuki/colorful_redstone_lamps/ColorfulRedstoneLampsClient.java
@@ -6,12 +6,13 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(value = ColorfulRedstoneLamps.MOD_ID, dist = Dist.CLIENT)
 public class ColorfulRedstoneLampsClient {
     public ColorfulRedstoneLampsClient(ModContainer container) {
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
-        NeoForge.EVENT_BUS.addListener(event -> TooltipHandler.onTooltip(event));
+        NeoForge.EVENT_BUS.addListener((ItemTooltipEvent event) -> TooltipHandler.onTooltip(event));
     }
 }
