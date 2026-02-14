331092fc846ede06c9fd8052e9c216910b1816ef
PasukiTV <Pascal-Smigielski@outlook.de>
2026-02-14 02:13:16 +0100

    Register tooltip listener explicitly on NeoForge event bus

--- src/main/java/de/pasuki/colorful_redstone_lamps/ColorfulRedstoneLampsClient.java
+++ src/main/java/de/pasuki/colorful_redstone_lamps/ColorfulRedstoneLampsClient.java
@@ -1,6 +1,7 @@
 package de.pasuki.colorful_redstone_lamps;
 
 import net.neoforged.api.distmarker.Dist;
+import de.pasuki.colorful_redstone_lamps.client.TooltipHandler;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -8,6 +9,7 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(
         value = "colorful_redstone_lamps",
@@ -19,6 +21,7 @@ import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 public class ColorfulRedstoneLampsClient {
     public ColorfulRedstoneLampsClient(ModContainer container) {
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+        NeoForge.EVENT_BUS.addListener(TooltipHandler::onTooltip);
     }
 
     @SubscribeEvent
--- src/main/java/de/pasuki/colorful_redstone_lamps/client/TooltipHandler.java
+++ src/main/java/de/pasuki/colorful_redstone_lamps/client/TooltipHandler.java
@@ -1,29 +1,38 @@
 package de.pasuki.colorful_redstone_lamps.client;
 
-import de.pasuki.colorful_redstone_lamps.data.ModItemTagsProvider;
+import de.pasuki.colorful_redstone_lamps.block.ModBlocks;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
 
-@Mod(
-        value = "colorful_redstone_lamps",
-        dist = {Dist.CLIENT}
-)
 public class TooltipHandler {
-    @SubscribeEvent
+    private TooltipHandler() {
+    }
+
     public static void onTooltip(ItemTooltipEvent e) {
         ItemStack stack = e.getItemStack();
-        if (!stack.isEmpty()) {
-            if (stack.is(ModItemTagsProvider.LAMPS)) {
-                e.getToolTip().add(Component.translatable("tooltip.colorful_redstone_lamps.lamp.normal").withStyle(ChatFormatting.GRAY));
-            } else if (stack.is(ModItemTagsProvider.INVERTED_LAMPS)) {
-                e.getToolTip().add(Component.translatable("tooltip.colorful_redstone_lamps.lamp.inverted").withStyle(ChatFormatting.GRAY));
-            }
-
+        if (stack.isEmpty()) {
+            return;
         }
+
+        Item item = stack.getItem();
+        if (isLampItem(item, false)) {
+            e.getToolTip().add(Component.translatable("tooltip.colorful_redstone_lamps.lamp.normal").withStyle(ChatFormatting.GRAY));
+        } else if (isLampItem(item, true)) {
+            e.getToolTip().add(Component.translatable("tooltip.colorful_redstone_lamps.lamp.inverted").withStyle(ChatFormatting.GRAY));
+        }
+    }
+
+    private static boolean isLampItem(Item item, boolean inverted) {
+        for (DeferredBlock<Block> deferredBlock : (inverted ? ModBlocks.INVERTED_LAMPS : ModBlocks.LAMPS).values()) {
+            if (deferredBlock.get().asItem() == item) {
+                return true;
+            }
+        }
+        return false;
     }
 }
